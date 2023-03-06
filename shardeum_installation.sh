f57dc5bb7c1d0c19be49fd47207e59ca026c3e68
Ivan Zaytsev <0x0043@gmail.com>
2023-03-06 17:20:55 +0300

    Update shardeum_installation.sh

--- shardeum_installation.sh
+++ shardeum_installation.sh
@@ -107,6 +107,17 @@ EOF
 
 RUNDASHBOARD=y
 
+while true; do
+  read -p "Set the password to access the Dashboard/Введите желаемый пароль для доступа к Dashboard: " -s input
+  echo
+  if [[ -n "$input" ]] && [[ ! "$input" =~ \  ]]; then
+    DASHPASS=$input
+    break
+  else
+    echo "Invalid input, try again./Неправильный ввод, попробуйте снова."
+  fi
+done
+
 echo -e "${fmt}\nSet port for node/Устанавливаем порт для ноды${end}" && sleep 1
 
 wget https://raw.githubusercontent.com/fackNode/shardeum/main/ports_cheker.sh && chmod +x ports_cheker.sh && ./ports_cheker.sh
@@ -206,7 +217,7 @@ cat >./.env <<EOL
 APP_IP=auto
 APP_SEEDLIST=${APPSEEDLIST}
 APP_MONITOR=${APPMONITOR}
-DASHPASS='123321'
+DASHPASS=${DASHPASS}
 DASHPORT=${DASHPORT}
 SERVERIP=${SERVERIP}
 LOCALLANIP=${LOCALLANIP}
